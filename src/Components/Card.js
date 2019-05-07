b15d45e26a6b394914808c699e72cb46dc2777b1
Rutenis <r.raila@keepthinking.it>
2019-05-07 11:26:37 +0100

    style updates

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,9 +7,9 @@ export class Card extends Component {
 
     return (        
         <a href={link}>
+            <img className='card__img' src={img} alt='' />
             <div className='card__title'>{title}</div>
-            <div className='card__description'>{description}</div>
-            <img src={img}/>
+            <div className='card__description'>{description}</div>            
         </a>
     )
   }
