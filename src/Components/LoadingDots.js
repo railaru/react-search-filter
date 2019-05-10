e9f92e3a8b5c385b57f1ff2aca2a04de50028e79
Rutenis <r.raila@keepthinking.it>
2019-05-10 16:41:53 +0100

    punk api, load more when scrolling, loading dots

---
+++ src/Components/LoadingDots.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function LoadingDots() {
+  return (
+    <div className="animated-loading-dots-wrapper">
+        <div className="animated-loading-dots">
+            <div className="dot1"></div>
+            <div className="dot2"></div>
+            <div className="dot3"></div>
+            <div className="dot4"></div>
+        </div>
+    </div>
+  )
+}
--- src/Components/SearchFilter.js
+++ src/Components/SearchFilter.js
@@ -3,14 +3,14 @@ import Fuse                 from 'fuse.js';
 import axios                from 'axios'
 
 import Card                 from './Card'
+import LoadingDots          from './LoadingDots'
 
-
-const  imageAddIncrement    = 12
-let    images               = 4
+const  imageAddIncrement    = 6
+let    images               = 12
 let    page                 = 1
 
 
-export class SearchFilter extends Component {
+export default class SearchFilter extends Component {
 
     constructor() {
         super()
@@ -22,18 +22,56 @@ export class SearchFilter extends Component {
         }
     }
 
+    onImagesLoadedToDOM(container, event) {
+        var images = container.querySelectorAll(".card__img");
+        var loaded = images.length;
+        for (var i = 0; i < images.length; i++) {
+            if (images[i].complete) {
+            loaded--;
+            } else {
+            // eslint-disable-next-line
+            images[i].onload = () => {
+                loaded--;
+                if (loaded === 0) {
+                event();
+                }
+            };
+            }
+            if (loaded === 0) {
+            event();
+            }
+        }
+    }
+
     fetchImages() {
         
         this.setState({ isLoading: true })
-
-        axios.get(`https://picsum.photos/v2/list?page=${page}&limit=${images}`) 
+        
+        axios.get(`https://api.punkapi.com/v2/beers?page=${page}&per_page=${images}`) 
         .catch((error) => console.log(error))
         .then((response) => {
-
+                            
             this.setState({ initialItems: response.data })
-            this.setState({ isLoading: false })
+    
             this.setState({ SearchResult: this.state.initialItems })
+            
+            this.onImagesLoadedToDOM(document.querySelector(".gallery-search-results"), () => {
+
+                this.setState({ isLoading: false })                                            
+            })        
+    
         })                    
+    }
+
+    isBottom() {
+        window.addEventListener('scroll', () => {
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+               
+               page ++
+               images = images + imageAddIncrement
+               this.fetchImages()
+            }
+         });
     }
 
     filterList = (e) => {
@@ -46,8 +84,8 @@ export class SearchFilter extends Component {
             maxPatternLength: 32,
             minMatchCharLength: 1,
             keys: [
-              "author",
-              "height",
+              "name",
+              "tagline",
             ]
           };
           
@@ -64,7 +102,8 @@ export class SearchFilter extends Component {
 
     componentDidMount() {
 
-        this.fetchImages()        
+        this.fetchImages()       
+        this.isBottom() 
     }
 
     render() {
@@ -83,20 +122,24 @@ export class SearchFilter extends Component {
 
                 <div className='gallery-search-results'>
                     {
-                        SearchResult.map((item) => (                        
+                        SearchResult.map(({ id, name, tagline, image_url }) => (                        
                             <Card
-                                key={item.id}
-                                title={item.author}
-                                description={item.height}
-                                img={item.download_url}
-                                link={item.url}
+                                key={id}
+                                link={image_url}
+                                img={image_url}
+                                title={name}                                
+                                description={tagline}                                
                             />                                             
                         ))
                     }
                 </div>
+                {
+                    this.state.isLoading &&
+                        <LoadingDots/>
+                }
             </div>
         )
     }
 }
 
-export default SearchFilter
+
